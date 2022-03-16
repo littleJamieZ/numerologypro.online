59564cc28cecf43bb4c2713c8f713bf8fbde83b4
Jamie <jamie1997120@gmail.com>
2022-03-16 14:40:24 -0400

    installs react-router-dom and set it up; creates routes for Home and LifePath components

--- numerologypro-app/src/App.jsx
+++ numerologypro-app/src/App.jsx
@@ -1,11 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import './App.css';
+import LifePath from './LifePath';
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">Numerologypro</header>
-			<Home />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="life-path" element={<LifePath />} />
+			</Routes>
 		</div>
 	);
 }
