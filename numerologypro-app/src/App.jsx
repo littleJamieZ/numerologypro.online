a393b5d0a5cbb32a545a6b569548ab4474fc6fe1
Jamie <jamie1997120@gmail.com>
2022-03-16 14:11:45 -0400

    adds Home component and home className to Home container; adds Home.css and import it into App

--- numerologypro-app/src/App.jsx
+++ numerologypro-app/src/App.jsx
@@ -1,9 +1,11 @@
+import Home from './Home';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">hello world</header>
+			<header className="App-header">Numerologypro</header>
+			<Home />
 		</div>
 	);
 }
